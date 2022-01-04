5171259d92e5105bf907ffd2305912b4ba9d83fd
Anton Sidorov <cohojet@gmail.com>
2022-01-04 17:56:08 +0300

    Add Day 2 solution

---
+++ Day2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PartOne();
+            PartTwo();
+
+            Console.ReadLine();
+        }
+
+        static void PartOne()
+        {
+            var course = File.ReadAllLines("input.txt");
+
+            var horizontal = 0;
+            var depth = 0;
+
+            foreach(var line in course)
+            {
+                var command = line.Split(' ');
+                var argument = int.Parse(command[1]);
+
+                switch(command[0])
+                {
+                    case "forward":
+                        horizontal += argument;
+                        break;
+                    case "down":
+                        depth += argument;
+                        break;
+                    case "up":
+                        depth -= argument;
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Part One result: {horizontal * depth} increases");
+        }
+
+        static void PartTwo()
+        {
+            var course = File.ReadAllLines("input.txt");
+
+            var horizontal = 0;
+            var depth = 0;
+            var aim = 0;
+
+            foreach (var line in course)
+            {
+                var command = line.Split(' ');
+                var argument = int.Parse(command[1]);
+
+                switch (command[0])
+                {
+                    case "forward":
+                        horizontal += argument;
+                        depth += aim * argument;
+                        break;
+                    case "down":
+                        aim += argument;
+                        break;
+                    case "up":
+                        aim -= argument;
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Part Two result: {horizontal * depth} increases");
+        }
+    }
+}
